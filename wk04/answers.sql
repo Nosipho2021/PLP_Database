de61189adfe281510733ce2eaf5c45486d655e98
Nosipho2021 <lorrainemdk@gmail.com>
2025-05-21 19:34:56 +0200

    Data manipulation

---
+++ wk04/answers.sql
@@ -0,0 +1,23 @@
+/* Question 1 */
+
+CREATE TABLE student(
+student_id int primary key,
+fullname varchar(100),
+age int
+);
+
+/* Question 2 */
+
+INSERT INTO student(student_id, fullname, age)
+VALUES(1, "Ama Boadu", 13),
+(2, "Kofi Mensah", 16),
+(3, "Kwasi Boakye", 14),
+(4, "Cystal Manuena", 13),
+(5, "Esi Agbesi", 15);
+
+/* Question 3 */
+
+UPDATE student 
+SET age = 20
+WHERE student_id = 2;
+
